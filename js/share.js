ec94c04ddaf2de0754ad40c1956721764d4ad9a1
ksugyeong04 <ksugyeong04@naver.com>
2024-06-11 00:58:08 +0900

    Add files via upload

--- js/share.js
+++ js/share.js
@@ -1,14 +1,14 @@
 const url = 'https://traaaveltest.netlify.app/';
-function setShare(){
-  var resulting = document.querySelector('#resultlmg');
+function setShare() {
+  var resultImg = document.querySelector('#resultlmg');
   var resultAlt = resultImg.firstElementChild.alt;
-  const shareTitle = '요기 어때 여행 성향 테스트 결과'
+  const shareTitle = '요기 어때 여행 성향 테스트 결과';
   const shareDes = infoList[resultAlt].name;
   const shareImage = url + 'img/image-' + resultAlt + '.png';
-  const shareURL = url +'page.result-' + resultAlt+ '.html';
+  const shareURL = url + 'page.result-' + resultAlt + '.html';
 }
 
-function kakaoShare(){ 
+function kakaoShare() {
   Kakao.Share.sendDefault({
     objectType: 'feed',
     content: {
@@ -19,42 +19,6 @@ function kakaoShare(){
         mobileWebUrl: shareURL,
         webUrl: shareURL,
       },
-    },
-    itemContent: {
-      profileText: 'Kakao',
-      profileImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-      titleImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-      titleImageText: 'Cheese cake',
-      titleImageCategory: 'Cake',
-      items: [
-        {
-          item: 'Cake1',
-          itemOp: '1000원',
-        },
-        {
-          item: 'Cake2',
-          itemOp: '2000원',
-        },
-        {
-          item: 'Cake3',
-          itemOp: '3000원',
-        },
-        {
-          item: 'Cake4',
-          itemOp: '4000원',
-        },
-        {
-          item: 'Cake5',
-          itemOp: '5000원',
-        },
-      ],
-      sum: '총 결제금액',
-      sumOp: '15000원',
-    },
-    social: {
-      likeCount: 10,
-      commentCount: 20,
-      sharedCount: 30,
     },
     buttons: [
       {
