9730c5ce1558b5a817bd9eb6a07c33205c019044
ksugyeong04 <ksugyeong04@naver.com>
2024-06-11 01:21:40 +0900

    Add files via upload

---
+++ share.js
@@ -0,0 +1,67 @@
+function kakaoShare(){
+  Kakao.Share.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: '요기 어때? 여행 테스트',
+      description: '재밌고 간단한 테스트를 통한 나만의 여행 메이트 찾기',
+      imageUrl:
+        'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+      link: {
+        mobileWebUrl: 'https://developers.kakao.com',
+        webUrl: 'https://developers.kakao.com',
+      },
+    },
+    itemContent: {
+      profileText: 'Kakao',
+      profileImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+      titleImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+      titleImageText: 'Cheese cake',
+      titleImageCategory: 'Cake',
+      items: [
+        {
+          item: 'Cake1',
+          itemOp: '1000원',
+        },
+        {
+          item: 'Cake2',
+          itemOp: '2000원',
+        },
+        {
+          item: 'Cake3',
+          itemOp: '3000원',
+        },
+        {
+          item: 'Cake4',
+          itemOp: '4000원',
+        },
+        {
+          item: 'Cake5',
+          itemOp: '5000원',
+        },
+      ],
+      sum: '총 결제금액',
+      sumOp: '15000원',
+    },
+    social: {
+      likeCount: 10,
+      commentCount: 20,
+      sharedCount: 30,
+    },
+    buttons: [
+      {
+        title: '웹으로 이동',
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+          webUrl: 'https://developers.kakao.com',
+        },
+      },
+      {
+        title: '앱으로 이동',
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+          webUrl: 'https://developers.kakao.com',
+        },
+      },
+    ],
+  });
+}
