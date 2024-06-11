c0d677b95d9c6059c7a6c82a193dae780ebd5a11
ksugyeong04 <ksugyeong04@naver.com>
2024-06-11 11:23:05 +0900

    Add files via upload

--- js/share.js
+++ js/share.js
@@ -5,7 +5,7 @@ function kakaoShare(){
       title: '요기 어때? 여행 테스트',
       description: '재밌고 간단한 테스트를 통한 나만의 여행 메이트 찾기',
       imageUrl:
-        'https://ibb.co/02ZkR8F',
+        'https://i.ibb.co/nmFdds7/test-image.jpg',
       link: {
         mobileWebUrl: 'https://developers.kakao.com',
         webUrl: 'https://developers.kakao.com',
