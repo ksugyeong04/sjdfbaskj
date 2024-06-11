be866d3d9cbb1d297b1658342d0895a61acfdf04
ksugyeong04 <ksugyeong04@naver.com>
2024-06-11 22:05:56 +0900

    Add files via upload

--- js/share.js
+++ js/share.js
@@ -1,13 +1,14 @@
 const url = 'https://traaaveltest.netlify.app/';
 
 function setShare(){
-  var resultImg = document.querySelectot('#resultImg')
+  var resultImg = document.querySelector('#resultImg');
   var resultAlt = resultImg.firstElementChild.alt;
-  const shareTilte = '여행 성향 결과'
+  const shareTilte = '여행 성향 결과';
   const shareDes = infoList[resultAlt].name;
   const shareImage = url + 'img/image-' + resultAlt +'.png';
   const shareURL = url + 'page.result-' + resultAlt +'.html';
-  kakaoShare
+
+  kakaoShare(shareTitle, shareDes, shareImage, shareURL);
 }
 
 function kakaoShare(){
